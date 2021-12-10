a9d34474be94b92d3258352ed6c43672a43bb8c5
Daisaku Kato <50142017+katkic@users.noreply.github.com>
2021-12-11 01:27:05 +0900

    ユーザー登録機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
   def index
-    @users = User.page(params[:page]).per(15)
+    @users = User.all.order(created_at: :desc).page(params[:page]).per(15)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  
+    if @user.save
+      redirect_to users_path, notice: "ユーザー 「#{@user.name}」を登録しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :address, :age)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, :email, :address, :age, presence: true
+  validates :age, numericality: true, inclusion: { in: 0..100, message: " : 0~100歳の範囲で入力してください" }, allow_blank: true
 end
