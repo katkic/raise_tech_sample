1608291ef3efa27cd01f82932a4f8a03f0668e88
Daisaku Kato <50142017+katkic@users.noreply.github.com>
2022-01-01 02:19:57 +0900

    ユーザー画像選択時のプレビュー表示を実装

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -5,4 +5,22 @@ $(document).on('turbolinks:load', function () {
       alert('最大ファイルサイズは5MB未満です。小さいファイルを選択してください。');
     }
   });
+  
+  // 新規作成・編集画面で、file_fieldに選択した画像をプレビューで確認できるようにする
+  function readURL(input) {
+    var bodyPicId = '#' + input.id + '_prev';
+
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $(bodyPicId).attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $('#user_picture').change(function () {
+    readURL(this);
+  });
 });
