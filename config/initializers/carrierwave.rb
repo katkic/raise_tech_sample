2eab6ee733d8ed596baf8588df309910708cbb37
Daisaku Kato <50142017+katkic@users.noreply.github.com>
2022-01-01 15:12:00 +0900

    production環境の設定変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,14 +5,14 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    region: 'ap-northeast-1',
-    # aws_access_key_id: ENV['S3_BUDGET_ACCESS_KEY'],
-    # aws_secret_access_key: ENV['S3_BUDGET_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_RAISE_TECH_SAMPLE_S3_REGION'],
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
     path_style: true
   }
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory = ENV['S3_BUDGET_NAME']
+  config.fog_directory = ENV['AWS_RAISE_TECH_SAMPLE_S3_BUCKET']
   # アップロードしたリソースを公開する場合はtrue
   config.fog_public = false
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
